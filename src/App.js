f406c198522bbf4dfeec60a299c3b65c64c8654f
Nati Kamusher <natan.kamusher@gmail.com>
2021-04-26 04:59:16 +0300

    started boilerplate project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 import { hot } from 'react-hot-loader';
-import TodoList from './todos/TodoList';
-
-const AppWrapper = styled.div`
-	margin: 1rem;
-	font-family: Arial, Helvetica, sans-serif;
-	color: #222222;
-`;
 
 const App = () => {
 	console.log('loading app');
-	return (
-		<AppWrapper>
-			<TodoList />
-		</AppWrapper>
-	);
+	return <p>Hello</p>;
 };
 
 export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from './App.js';
-import { Provider } from 'react-redux';
-import { configureStore } from './store';
 
-const store = configureStore();
-const persistor = persistStore(store);
-
-ReactDOM.render(
-	<Provider store={store}>
-		<PersistGate persistor={persistor} loading={<div>Loading...</div>}>
-			<App />
-		</PersistGate>
-	</Provider>,
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
